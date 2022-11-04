3f0419f792028cf615ddaf60755e557f910ee342
soumyamekala <116692351+soumyamekala@users.noreply.github.com>
2022-11-03 22:52:44 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Trying Web hooks *****-----");
+		System.out.println("-----***** Web hooks *****-----");
 	}
 }
